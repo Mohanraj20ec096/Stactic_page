703b58d9213b4bc2db0ededcebea59413ac064ff
Mohanraj20ec096 <mohnarajm@gmail.com>
2022-12-22 21:18:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+//import img from './tabbg.jpg';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table bgcolor='white'>
+        <tr background-color='white' >
+          <td>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIaZoZmN__B5SI9Xg9f0liNpNew2_gXTR7rA&usqp=CAU' alt='Logo' height={100} width={100
+            }></img>       
+          </td>
+          <td>
+          <div class="topnav">
+          <a href="#home">Home</a>
+          <a href="#contact">About Us</a>
+          </div>
+          </td>
+        </tr>
+      </table>
+      <table bgcolor='#F0E9E7' width='100%' >
+        <tr align='left' >
+          <td>
+            <h1 align='left'>Embedded System for Absolute begginers</h1>
+            <p align='left'>(MCU1) Learn bare metal driver development using Embedded C: Writing drivers for STM32 GPIO,I2C,SPI,USART from scratch With hands on coding using C Programming and assembly on ARM Cortex M Processor based Microcontroller</p><br></br>
+            <p align='left'>Course Instructor:Mr.ABC</p>
+            Star Rating : 
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <br></br><br></br>
+            &ensp;Rs.999&nbsp;<s>Rs.2000</s><br></br>
+            <br></br>
+            &ensp;<button type='button' className='button'>Buy Now</button>
+          </td>
+          <td><br></br>
+            <img src="https://res.cloudinary.com/rs-designspark-live/image/upload/c_limit,w_595/f_auto/v1/article/What_is_an_embedded_system_9973b860b8142db0b5b4290e405d38cadb0cc242" alt='Course' height={350} width={350} className="img">
+            </img>
+          </td>
+        </tr>
+        
+      </table>
+      <table bgcolor='#F0E9E7 ' width='100%'  >
+      <tr align='left' >
+          <td>
+            <h1 align='left'>What you'll Learn</h1>
+            <ul style={{listStyle:'square',fontWeight:'bold'}}>
+            <li>Understand Right ways of Handling and programming MCU Peripherals</li><br></br> 
+              <li>Understand complete Driver Development steps right from scratch for GPIO,SPI,I2C and USART.</li><br></br> 
+              <li>Explore MCU data sheets, Reference manuals, start-up Codes to get things done</li><br></br> 
+              <li>Demystifying behind the scene working details of SPI,I2C,GPIOs,USART etc.</li><br></br> 
+              <li>Learn about Peripheral IRQs/Vector table/NVIC interfaces and many</li><br></br> 
+              <li>Understand right ways of enabling/configuring peripheral clocks/serial clocks/baud rates of various serial protocols</li><br></br>             
+            </ul>
+            </td>
+        </tr>
+
+        <tr align='left' >
+          <td>
+            <h1 align='left'>Description</h1>
+            <p>
+            <b>Latest Update:</b>  
+ 
+ Bare metal embedded , writing linker script and startup file from scratch. 
+  
+ processor fault analysis and handling, stacks, AAPCS explanation. 
+  
+ Implementation of simple task scheduler using pendsv and systick 
+  
+ GCC inline assembly and naked functions 
+  
+  
+  
+ More than 10000+ students have already taken this course with More than 900+ happy reviews. 
+  
+ Have you ever tried to learn about the ARM Cortex M3/M4 Processor by reading a book ....</p>
+<button type='button' className='button'>Read more</button>
+            
+            </td>
+        </tr>
+      </table>
+
+      <footer>
+      <table bgcolor='#ddd' width='100%'>
+      <tr>
+      <td width="800px" height="50px" colspan="4"></td>
+      </tr>
+      <tr >
+      <td height="200px" width="300px" align="center">
+        <ul style={{listStyle:'none'}} >
+          <li>A2Z business</li><br></br>
+		<li> Teach on A2Z</li><br></br>
+		<li>Get The App</li><br></br>
+		<li> About Us</li><br></br>
+		<li>Investors</li><br></br>
+
+</ul></td>
+<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}}><li>Carrer</li><br></br>
+		<li> Blog</li><br></br>
+		<li>Help</li><br></br>
+		<li>Affiliate</li><br></br>
+		<li>Accessibility</li><br></br>
+
+</ul></td>
+<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}} ><li>Terms</li><br></br>
+		<li> Privacy policy and cookie policy</li><br></br>
+		<li>Topics/concepts</li><br></br>
+		<li>sitemap</li><br></br>
+		<li>Features</li><br></br>
+
+</ul></td>	
+	<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}}><li>Follow US</li><br></br>
+		<li> <img src="https://cdn-icons-png.flaticon.com/512/37/37409.png?w=740&t=st=1671602413~exp=1671603013~hmac=e2223c669fa836fcc43c026b0adb21dc0ab886a9db1eb23f52a652e33e683e0a" alt="img" height="12px" width="12px"></img>&ensp;Facebook</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/37/37405.png?w=740&t=st=1671602440~exp=1671603040~hmac=88e7e3e6ccdf296cba706fbf8af0db43e5e033449b7ff9610864ab34ae01efff" alt="img" height="12px" width="12px"></img>&ensp;Instagram</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/1051/1051333.png?w=740&t=st=1671602587~exp=1671603187~hmac=53b2d620088402129c29b4b55160e8fbb884dd0b1fc338d38eb96dc3fc505bbd" alt="img" height="12px" width="12px"></img>&ensp;Linkedin</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/39/39552.png?w=740&t=st=1671602333~exp=1671602933~hmac=a740faeb1588c9431205086bef279f7a19cb3493b6260e128dd8de74c640e609" alt="img" height="12px" width="12px"></img>&ensp;Twitter</li><br></br>
+
+</ul></td>
+
+
+
+      </tr>
+      </table>
+      </footer>
+
+
     </div>
+
+
+
+
+
+
+
+
+
+
   );
 }
 
 export default App;
+
